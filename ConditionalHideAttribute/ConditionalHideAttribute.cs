fb9dd0e263907ad1071c0869c941b7ade9c0afe6
whisperlin <309762472@qq.com>
2021-01-19 11:37:39 +0800

    属性约束升级

--- ConditionalHideAttribute/ConditionalHideAttribute.cs
+++ ConditionalHideAttribute/ConditionalHideAttribute.cs
@@ -13,7 +13,7 @@ public class ConditionalHidePropertyDrawer : PropertyDrawer
         bool enabled = true;
         //Look for the sourcefield within the object that the property belongs to
         string propertyPath = property.propertyPath; //returns the property path of the property we want to apply the attribute to
-        string conditionPath = propertyPath.Replace(property.name, condHAtt.ConditionalSourceField); //changes the path to the conditionalsource property path
+        string conditionPath = propertyPath.Replace(property.name, condHAtt.conditionalSourceField); //changes the path to the conditionalsource property path
         SerializedProperty sourcePropertyValue = property.serializedObject.FindProperty(conditionPath);
 
         if (sourcePropertyValue != null)
@@ -43,6 +43,51 @@ public class ConditionalHidePropertyDrawer : PropertyDrawer
         }
        
     }
+    void CommonProperty(Rect position,SerializedProperty property)
+    {
+        if (property.propertyType == SerializedPropertyType.Enum)
+        {
+            var type = property.serializedObject.targetObject.GetType();
+            var field = type.GetField(property.name);
+            var enumtype = field.FieldType;
+            List<string> m_displayNames = new List<string>();
+            foreach (var enumName in property.enumNames)
+            {
+                var enumfield = enumtype.GetField(enumName);
+                var hds = enumfield.GetCustomAttributes(typeof(HeaderAttribute), false);
+                m_displayNames.Add(hds.Length <= 0 ? enumName : ((HeaderAttribute)hds[0]).header);
+            }
+            EditorGUI.BeginChangeCheck();
+            var value = EditorGUI.Popup(position, property.name, property.enumValueIndex, m_displayNames.ToArray());
+            if (EditorGUI.EndChangeCheck())
+            {
+                property.enumValueIndex = value;
+            }
+        }
+        else if (property.isArray)
+        {
+            ArrayGUI( position,property);
+        }
+        else
+        {
+
+            EditorGUI.PropertyField(position, property);
+        }
+    }
+    void ArrayGUI(Rect position, SerializedProperty property)
+    {
+        SerializedProperty arraySizeProp = property.FindPropertyRelative("Array.size");
+        EditorGUILayout.PropertyField(arraySizeProp);
+
+        EditorGUI.indentLevel++;
+
+        for (int i = 0; i < arraySizeProp.intValue; i++)
+        {
+            CommonProperty(position, property.GetArrayElementAtIndex(i));
+            EditorGUILayout.PropertyField(property.GetArrayElementAtIndex(i));
+        }
+        EditorGUI.indentLevel--;
+    }
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
         //get the attribute data
@@ -59,8 +104,47 @@ public class ConditionalHidePropertyDrawer : PropertyDrawer
         if ( enabled)
         {
             label.text = condHAtt.Label;
+            if (condHAtt.type0 != null  )
+            {
+                var enumtype = condHAtt.type0;
+                string[] enumNames = System.Enum.GetNames(enumtype);
+                int selectIndex = 0;
 
-            if (condHAtt.condiction)
+                System.Array values = System.Enum.GetValues(enumtype);
+                List<int> _values = new List<int>();
+                foreach (var v in values)
+                {
+                    int _v = (int)v;
+                    _values.Add(_v);
+
+
+                }
+                for (int i = 0; i < _values.Count; i++)
+                {
+                    if (_values[i] == property.intValue)
+                    {
+                        selectIndex = i;
+                    }
+                }
+
+                List<string> m_displayNames = new List<string>();
+
+                foreach (var enumName in enumNames)
+                {
+                    var enumfield = enumtype.GetField(enumName);
+                    var hds = enumfield.GetCustomAttributes(typeof(HeaderAttribute), false);
+                    m_displayNames.Add(hds.Length <= 0 ? enumName : ((HeaderAttribute)hds[0]).header);
+                }
+
+
+                EditorGUI.BeginChangeCheck();
+                var _index = EditorGUI.Popup(position, label.text, selectIndex, m_displayNames.ToArray());
+                if (EditorGUI.EndChangeCheck())
+                {
+                    property.intValue = _values[_index];
+                }
+            }
+            else if (condHAtt.condiction)
             {
                 if (property.propertyType == SerializedPropertyType.Float)
                 {
@@ -70,7 +154,6 @@ public class ConditionalHidePropertyDrawer : PropertyDrawer
                 {
                     EditorGUI.IntSlider(position, property, (int)condHAtt.min, (int)condHAtt.max, label);
                 }
-                
                 else
                 {
                     EditorGUI.PropertyField(position, property, label, true);
@@ -81,25 +164,44 @@ public class ConditionalHidePropertyDrawer : PropertyDrawer
 
                 if (property.propertyType == SerializedPropertyType.Enum)
                 {
+                    var _type = property.GetType();
+ 
                     var type = property.serializedObject.targetObject.GetType();
                     var field = type.GetField(property.name);
-                    var enumtype = field.FieldType;
-                    List<string> m_displayNames = new List<string>();
-                    foreach (var enumName in property.enumNames)
+                    if (null == field || null == field.FieldType)
                     {
-                        var enumfield = enumtype.GetField(enumName);
-                        var hds = enumfield.GetCustomAttributes(typeof(HeaderAttribute), false);
-                        m_displayNames.Add(hds.Length <= 0 ? enumName : ((HeaderAttribute)hds[0]).header);
+                        
+
+                        EditorGUI.BeginChangeCheck();
+                        var value = EditorGUI.Popup(position, label.text, property.enumValueIndex, property.enumDisplayNames);
+                        if (EditorGUI.EndChangeCheck())
+                        {
+                            property.enumValueIndex = value;
+                        }
                     }
-                    EditorGUI.BeginChangeCheck();
-                    var value = EditorGUI.Popup(position, label.text, property.enumValueIndex, m_displayNames.ToArray());
-                    if (EditorGUI.EndChangeCheck())
+                    else
                     {
-                        property.enumValueIndex = value;
+                        var enumtype = field.FieldType;
+                        List<string> m_displayNames = new List<string>();
+                        foreach (var enumName in property.enumNames)
+                        {
+                            var enumfield = enumtype.GetField(enumName);
+                            var hds = enumfield.GetCustomAttributes(typeof(HeaderAttribute), false);
+                            m_displayNames.Add(hds.Length <= 0 ? enumName : ((HeaderAttribute)hds[0]).header);
+                        }
+                        EditorGUI.BeginChangeCheck();
+                        var value = EditorGUI.Popup(position, label.text, property.enumValueIndex, m_displayNames.ToArray());
+                        if (EditorGUI.EndChangeCheck())
+                        {
+                            property.enumValueIndex = value;
+                        }
+                     
                     }
+                    
                 }
                 else
                 {
+
                     EditorGUI.PropertyField(position, property, label, true);
                 }
                 
@@ -119,30 +221,34 @@ public class ConditionalHidePropertyDrawer : PropertyDrawer
 public class LabelAttribute : PropertyAttribute
 {
     //The name of the bool field that will be in control
-    public string ConditionalSourceField = "";
+    public string conditionalSourceField = "";
     public string Label = "";
     public bool condiction = false;
     public float max;
     public float min;
-    public bool ctrlByParam = true; 
+    public bool ctrlByParam = true;
 
+ 
+    public System.Type type0 = null;
     public LabelAttribute(string label,string conditionalSourceField)
     {
-        this.ConditionalSourceField = conditionalSourceField;
+        this.conditionalSourceField = conditionalSourceField;
       
         this.Label = "    "+label;
         condiction = false;
         ctrlByParam = true;
+        type0 = null;
     }
 
     public LabelAttribute(string label, string conditionalSourceField,float min,float max)
     {
         this.Label = "    "+label;
-        this.ConditionalSourceField = conditionalSourceField;
+        this.conditionalSourceField = conditionalSourceField;
         condiction = true;
         this.max = max;
         this.min = min;
         this.ctrlByParam = true;
+        type0 = null;
     }
     public LabelAttribute(string label, float min, float max)
     {
@@ -151,6 +257,7 @@ public class LabelAttribute : PropertyAttribute
         this.max = max;
         this.min = min;
         this.ctrlByParam = false;
+        type0 = null;
     }
 
     public LabelAttribute(string label )
@@ -158,6 +265,16 @@ public class LabelAttribute : PropertyAttribute
         this.Label = label;
         condiction = false;
         this.ctrlByParam = false;
+        type0 = null;
+    }
+
+
+    public LabelAttribute(string label,System.Type t)
+    {
+        this.Label = label;
+        condiction = false;
+        this.ctrlByParam = false;
+        type0 = t;
     }
 
 }
